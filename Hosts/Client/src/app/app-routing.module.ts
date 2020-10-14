1b5973027151e7a2da23131dd649043a3b192deb
XUANPHI <32946693+xuanphi7895@users.noreply.github.com>
2020-10-14 08:45:53 +0700

    Add header title

--- Hosts/Client/src/app/app-routing.module.ts
+++ Hosts/Client/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
-import { ProductDetailsComponent } from './shop/product-details/product-details.component';
-import { ShopComponent } from './shop/shop.component';
+import { TestErrorComponent } from './core/test-error/test-error.component';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
+  {path: 'test-error', component: TestErrorComponent},
    {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mode => mode.ShopModule)},
   // {path: 'shop/:id', component: ProductDetailsComponent},
   {path: '**', redirectTo: '', pathMatch: 'full'}
--- Hosts/Client/src/app/core/core.module.ts
+++ Hosts/Client/src/app/core/core.module.ts
@@ -2,15 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
+import { TestErrorComponent } from './test-error/test-error.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { ServerErrorComponent } from './server-error/server-error.component';
+import { ToastrModule } from 'ngx-toastr';
+import { SectionHeaderComponent } from './section-header/section-header.component';
 
 
 
 @NgModule({
-  declarations: [NavBarComponent],
+  declarations: [NavBarComponent, TestErrorComponent, NotfoundComponent, ServerErrorComponent, SectionHeaderComponent],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true
+    })
   ],
-  exports: [NavBarComponent]
+  exports: [
+    NavBarComponent,
+    SectionHeaderComponent
+  ]
 })
 export class CoreModule { }
