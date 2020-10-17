55cb22bd96477f3b1b61d4fd4ca7f3ba0ba4aa0a
XUANPHI <32946693+xuanphi7895@users.noreply.github.com>
2020-10-17 09:17:02 +0700

    Style for product

--- Hosts/Client/src/app/app-routing.module.ts
+++ Hosts/Client/src/app/app-routing.module.ts
@@ -2,13 +2,18 @@ import { TestErrorComponent } from './core/test-error/test-error.component';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ServerErrorComponent } from './core/server-error/server-error.component';
+import { NotfoundComponent } from './core/notfound/notfound.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'test-error', component: TestErrorComponent},
-   {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mode => mode.ShopModule)},
+  {path: '', component: HomeComponent, data: {breadcrumb: 'Home'}},
+  {path: 'test-error', component: TestErrorComponent, data: {breadcrumb: 'Test-Error'}},
+  {path: 'server-error', component: ServerErrorComponent, data: {breadcrumb: 'Server-Error'}},
+  {path: 'not-found', component: NotfoundComponent, data: {breadcrumb: 'Not-Found'}},
+  
+   {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mode => mode.ShopModule), data:{breadcrumb: 'Shop'}},
   // {path: 'shop/:id', component: ProductDetailsComponent},
-  {path: '**', redirectTo: '', pathMatch: 'full'}
+  {path: '**', redirectTo: 'not-found', pathMatch: 'full'}
 ];
 
 @NgModule({
