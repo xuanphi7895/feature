16fdfc70e4191b88874c4510d1d4d4335ce4d6dc
XUANPHI <32946693+xuanphi7895@users.noreply.github.com>
2020-10-31 03:07:43 +0700

    add model basket

--- Hosts/Client/src/app/app-routing.module.ts
+++ Hosts/Client/src/app/app-routing.module.ts
@@ -10,8 +10,8 @@ const routes: Routes = [
   {path: 'test-error', component: TestErrorComponent, data: {breadcrumb: 'Test-Error'}},
   {path: 'server-error', component: ServerErrorComponent, data: {breadcrumb: 'Server-Error'}},
   {path: 'not-found', component: NotfoundComponent, data: {breadcrumb: 'Not-Found'}},
-  
    {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mode => mode.ShopModule), data:{breadcrumb: 'Shop'}},
+   {path: 'basket', loadChildren: () => import('./basket/basket.module').then(mode => mode.BasketModule), data:{breadcrumb: 'Basket'}},
   // {path: 'shop/:id', component: ProductDetailsComponent},
   {path: '**', redirectTo: 'not-found', pathMatch: 'full'}
 ];
