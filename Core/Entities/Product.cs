123e276e1ddb6b81bf513f5ade592c22b7d547b9
XUANPHI <32946693+xuanphi7895@users.noreply.github.com>
2020-11-15 23:56:10 +0700

    add package for project

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -6,12 +6,12 @@ namespace Core.Entities
         public string Name {get; set;}
         public string Description {get;set;}
         public decimal Price {get;set;}
-         public decimal OriginalPrice {get;set;}
+       //  public decimal OriginalPrice {get;set;}
         public string PictureUrl {get;set;}
-        public int Stock {get;set;}
-        public int ViewCount {get;set;}
-        public DateTime DateCreated {get;set;}
-        public string SeoAlias {get;set;}
+        //public int Stock {get;set;}
+        //public int ViewCount {get;set;}
+        //public DateTime DateCreated {get;set;}
+        //public string SeoAlias {get;set;}
         public ProductType ProductType {get;set;}
         public int ProductTypeId {get;set;}
         public ProductBrand ProductBrand {get;set;}
