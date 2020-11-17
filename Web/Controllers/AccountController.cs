30b86fbead1c5e1b17ee7f7fa220b860e010a5aa
XUANPHI <32946693+xuanphi7895@users.noreply.github.com>
2020-11-17 12:27:26 +0700

    Add api login in account controller

---
+++ Web/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+using System.ComponentModel;
+using System.Net.Mime;
+using Microsoft.AspNetCore.Identity;
+using Core.Entities.Identity;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Web.DTOs;
+using Web.Errors;
+
+namespace Web.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+           _signInManager = signInManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto){
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+            if (user == null) return Unauthorized(new ApiResponse(401));
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if (!result.Succeeded) return Unauthorized(new ApiResponse(401));
+
+            return new UserDto
+            {
+                Email = user.Email,
+                Token = "This is be a token",
+                UserName = user.DisplayName
+            };
+        }
+    }
+}
