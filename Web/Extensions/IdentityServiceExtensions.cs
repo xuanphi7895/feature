618a7133a902369ff4ff1796a704fd98b665b3bf
XUANPHI <32946693+xuanphi7895@users.noreply.github.com>
2020-11-24 06:47:18 +0700

    add api for account controller

--- Web/Extensions/IdentityServiceExtensions.cs
+++ Web/Extensions/IdentityServiceExtensions.cs
@@ -1,18 +1,37 @@
+using System.Security.AccessControl;
+using System.Security.Principal;
 using Core.Entities.Identity;
 using Infrastructure.Identity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Web.Extensions
 {
     public static class IdentityServiceExtensions
     {
-        public static IServiceCollection AddIdentityServices(this IServiceCollection services){
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config){
+
             var builder = services.AddIdentityCore<AppUser>();
             builder = new IdentityBuilder(builder.UserType, builder.Services);
             builder.AddEntityFrameworkStores<AppIdentityDbContext>();
             builder.AddSignInManager<SignInManager<AppUser>>();
-            services.AddAuthentication();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(token => {
+                    token.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        //Same Secret key will be used while creating the token
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(config["Token:Key"])),
+                        ValidateIssuer = true,
+                        ValidIssuer = config["Token:Issuer"],
+                        ValidateAudience = false
+                    };
+                });
             return services;
 
         }
